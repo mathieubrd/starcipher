3fe58257cb13e2d70f454e44aa04dda093cd48ea
Mathieu Brochard <mathieu.brochard@gmx.fr>
2016-10-26 14:41:08 +0200

    Add affine encryption, decryption and unit tests

---
+++ starcipher/affine.py
@@ -0,0 +1,46 @@
+import re
+
+# Utility class to encrypt, decrypt and crack messages using affine cipher.
+class Affine:
+
+    # Returns True if the number passed in a prime number.
+    def is_prime(n):
+        for i in range(2, n):
+            return pow(i, n - 1) % n == 1
+        return True
+
+    # Returns the Bézout coefficients and the gcd of the two given numbers.
+    def xgcd(a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            g, x, y = Affine.xgcd(b % a, a)
+            return (g, y - (b // a) * x, x)
+
+    # Returns the modular multiplicative inverse of n mod m
+    def mod_inv(n, m):
+        g, x, _ = Affine.xgcd(n, m)
+        if g == 1:
+            return x % m
+
+    # Encrypt the a given message using the two given keys.
+    # The first key must be coprime with 26 (the length of the alphabet).
+    # Returns the encrypted message.
+    def encrypt(key1, key2, message):
+        if not Affine.is_prime(key1):
+            raise Exception('First key must be a prime number.')
+        message = list(message.upper())
+        for i in range(0, len(message)):
+            if re.compile('[A-Z]').match(message[i]):
+                message[i] = chr(((key1 * (ord(message[i]) - 65) + key2) % 26) + 65)
+        return "".join(message)
+
+    # Decrypt (if it is possible) the given message using the two given keys.
+    # Returns the potentially decrypted message.
+    def decrypt(key1, key2, message):
+        k = Affine.mod_inv(key1, 26)
+        message = list(message.upper())
+        for i in range(0, len(message)):
+            if re.compile('[A-Z]').match(message[i]):
+                message[i] = chr((k * (ord(message[i]) - key2 - 65) % 26) + 65)
+        return ''.join(message)
---
+++ tests/test_affine.py
@@ -0,0 +1,36 @@
+import unittest
+from starcipher.affine import Affine
+
+class TestAffine(unittest.TestCase):
+
+    def test_xgcd(self):
+        self.assertEqual(Affine.xgcd(7, 3), (1, 1, -2))
+        self.assertEqual(Affine.xgcd(14812, 1482), (2, 185, -1849))
+
+    def test_mod_inv(self):
+        self.assertEqual(Affine.mod_inv(7841, 41), 37)
+        self.assertNotEqual(Affine.mod_inv(451, 78), 1)
+
+    def test_is_prime(self):
+        self.assertEqual(Affine.is_prime(1), True)
+        self.assertEqual(Affine.is_prime(2), True)
+        self.assertEqual(Affine.is_prime(3), True)
+        self.assertEqual(Affine.is_prime(4), False)
+        self.assertEqual(Affine.is_prime(5), True)
+        self.assertEqual(Affine.is_prime(7), True)
+        self.assertEqual(Affine.is_prime(8), False)
+
+    def test_inv_mod(self):
+        self.assertEqual(Affine.mod_inv(3, 26), 9)
+
+    def test_encrypt(self):
+        self.assertEqual(
+            Affine.encrypt(3, 12, 'HEY, THIS IS THE AFFINE CIPHER!'),
+            'HYG, RHKO KO RHY MBBKZY SKFHYL!'
+        )
+
+    def test_decrypt(self):
+        self.assertEqual(
+            Affine.decrypt(3, 12, 'HYG, RHKO KO RHY MBBKZY SKFHYL!'),
+            'HEY, THIS IS THE AFFINE CIPHER!'
+        )
